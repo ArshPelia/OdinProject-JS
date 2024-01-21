3d97857afdb8e4156c1538e98fb512a21e067830
arshjit pelia <arsh.pelia1@gmail.com>
2024-01-21 18:26:13 -0500

    updated to etch a sketch

--- Projects/Fundementals/Etch-a-Sketch/index.js
+++ Projects/Fundementals/Etch-a-Sketch/index.js
@@ -1,3 +1,40 @@
 console.log("Script init...")
 
+// let grid = [];
+// const container = document.querySelector('#container');
 
+// creategrid();
+
+// console.log(grid);
+
+// function creategrid() {
+//     for (let x = 0; x < 16; x++) {
+//     				grid[x] = [];
+//         for (let y = 0; y < 16; y++) {
+//             addCell(x, y); 
+//         }
+//     }
+// }
+
+// function addCell(x, y) {
+//     // const newDiv = document.createElement("div");
+//     grid[x][y] = cell(x,y); // create a new object on x and y
+//     // grid[x][y] = newDiv; // create a new object on x and y
+// }
+
+// function cell(x,y) {
+// 	return (x+1)+":"+(y+1);
+// }
+
+const container = document.querySelector('#container');
+
+function makeDivs(numDivs) {
+    for (let d = 0; d < numDivs; d++){
+        let cells = document.createElement('div');
+        cells.className = "cell"
+        cells.textContent = d
+        container.appendChild(cells)    
+        }
+}
+
+makeDivs(256);
--- Projects/Fundementals/RPS/index.js
+++ Projects/Fundementals/RPS/index.js
@@ -1,8 +1,22 @@
 console.log("Script init...")
 
+const dict = {
+    1: "Rock",
+    2: "Paper",
+    3: "Scissors"
+  };
 
 function getComputerChoice(){
-    return Math.floor(Math.random() * 3);
+    var x = Math.floor(Math.random() * 3);
+    return dict[x]
 }
-
-function Play(playerSelection, computerSelection )
+function playRound(playerSelection, computerSelection) {
+    // your code here!
+    if(playerSelection.toUpperCase() === computerSelection.toUpperCase()) {
+        console.log("Tie")
+    }
+}
+   
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
