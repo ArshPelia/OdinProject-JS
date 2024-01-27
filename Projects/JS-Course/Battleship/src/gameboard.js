2804a080b9829988b63b8f78f52c0eb63ad4b97c
arshjit pelia <arsh.pelia1@gmail.com>
2024-01-27 18:28:45 -0500

    added cell to board in bs

--- Projects/JS-Course/Battleship/src/gameboard.js
+++ Projects/JS-Course/Battleship/src/gameboard.js
@@ -1,5 +1,32 @@
 class Gameboard{
+    rows = 10
+    cols = 10
+    board = []
     constructor(){
-        
+        this.rows= rows
+        this.cols = cols
+        this.board = board
+        //create empty board
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+            for (let j = 0; j < columns; j++) {
+              board[i].push(Cell());
+            }
+          }
     }
+    //print board to console
+    printToConsole(){
+        const boardWithCellValues = this.board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+    }
+}
+
+class Cell {
+    x;
+    y;
+    occupiedBy = null;
+    constructor(){
+        this.occupiedBy = occupiedBy;
+    }
+    
 }
