f52e27706315063d4a0949c98d1d7c21d127d572
arshjit pelia <arsh.pelia1@gmail.com>
2024-01-27 16:11:15 -0500

    started api calls in weather

--- Projects/JS-Course/Weather/src/index.js
+++ Projects/JS-Course/Weather/src/index.js
@@ -1,3 +1,25 @@
 console.log("Script Started")
 
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelector('#form').onsubmit = function(event) {
+        event.preventDefault(); // Prevent the default form submission behavior
+        geoCode();
+    };
+});
+
+async function geoCode() {
+    const loc = document.querySelector('#location').value;
+    console.log("Calling Geocode API for: " + loc);
+    try{
+        const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${loc}&limit={2}&appid=2373a86cb8c375c78b4a4391d4343a58`, 
+            {mode: 'cors'});
+        const data = await response.json();
+        console.log("Lat: " + data.lat)
+        console.log("Lon: " + data.lon)
+    }catch(error){
+        console.error(error)
+    }
+
+}
+
 console.log("Script Ended")
