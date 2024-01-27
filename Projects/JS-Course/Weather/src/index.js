59a8c2626a671969bcb5ff34d76e2a5f7d3e707c
arshjit pelia <arsh.pelia1@gmail.com>
2024-01-27 16:58:02 -0500

    got weather working with console log

--- Projects/JS-Course/Weather/src/index.js
+++ Projects/JS-Course/Weather/src/index.js
@@ -16,11 +16,7 @@ async function geoCode() {
 
         // Check if there are results
         if (data.length > 0) {
-            // console.log("Data:", data[0]);
-            // console.log("Lat:", data[0].lat);
-            // console.log("Lon:", data[0].lon);
-            getWeather(loc, data[0].lat, data[0].lon)
-
+            getWeather(loc, data[0].lat, data[0].lon);
         } else {
             console.log("No results found");
         }
@@ -30,16 +26,14 @@ async function geoCode() {
 }
 
 async function getWeather(loc, lat, lon) {
-    const loc = document.querySelector('#location').value;
     console.log("Calling Weather API for: " + loc + " at: " + lat + lon );
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=2373a86cb8c375c78b4a4391d4343a58`, { mode: 'cors' });
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=2373a86cb8c375c78b4a4391d4343a58`, { mode: 'cors' });
         const data = await response.json();
 
         console.log("Data: ", data);
         console.log("Timezone: ", data.timezone);
-        // console.log("Lon:", data[0].lon);
-
+        console.log("Weather: " + data.weather[0].description)
     } catch (error) {
         console.error(error);
     }
