eceb5ad66beff0b4fe23a05ebd91a343bb095a6e
arshjit pelia <arsh.pelia1@gmail.com>
2024-02-03 17:01:19 -0500

    setup api calls in cart

--- Projects/React-Course/Cart/cart-app/src/components/Home.jsx
+++ Projects/React-Course/Cart/cart-app/src/components/Home.jsx
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react"
+
 const Home = () => {
+    const [products, setProducts] = useState([])
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch('https://fakestoreapi.com/products?limit=5', {mode: "cors"})
+        .then((response) => {
+            if (response.status >= 400) {
+                throw new Error("server error");
+              }
+              return response.json();
+            })
+        .then((response) => setProducts(response.json))
+        .catch((error) => setError(error))
+        .finally(() => setLoading(false));
+    }, []);
+
+    if (error) return <p>A network error was encountered</p>;
+    if (loading) return <p>Loading...</p>;
+
+
     return(
-        <div id="container">
+        <div id="content">
             <h1>Home Page</h1>
         </div>
     )
