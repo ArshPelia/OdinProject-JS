81537d0463b66f21ddb65013295b0fbe20274c2d
arshjit pelia <arsh.pelia1@gmail.com>
2024-03-09 14:22:02 -0500

    created messages on index for msg-board

--- Projects/NodeJS-course/msg-board/routes/index.js
+++ Projects/NodeJS-course/msg-board/routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const msgs = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log(msgs)
+  res.render('index', { title: 'Express', messages: msgs });
 });
 
 module.exports = router;
